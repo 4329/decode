0fcb6b9b1569441666084dfe83cd8f655396fd32
awall <andrew.wall@stem2u.org>
2025-11-10 23:19:53 -0600

    logging shooter stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/ShootSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/ShootSubsystem.java
@@ -7,6 +7,8 @@ import static org.firstinspires.ftc.teamcode.util.RobotConfig.SHOOTER_I;
 import static org.firstinspires.ftc.teamcode.util.RobotConfig.SHOOTER_P;
 import static org.firstinspires.ftc.teamcode.util.RobotConfig.SHOOTER_PERCENT;
 
+import android.util.Log;
+
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
@@ -27,6 +29,7 @@ public class ShootSubsystem extends SubsystemBase {
     }
 
     private void setUp() {
+        shooterMotor.setInverted(true);
         shooterMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
         shooterMotor.setRunMode(Motor.RunMode.VelocityControl);
         shooterMotor.setVeloCoefficients(SHOOTER_P, SHOOTER_I, SHOOTER_D);
@@ -46,6 +49,8 @@ public class ShootSubsystem extends SubsystemBase {
     public void periodic() {
         if (running) {
             shooterMotor.set(SHOOTER_PERCENT);
+
+            Log.i("pct/MAX_TICKS/vel/accel", String.format("%f, %f, %f, %f", SHOOTER_PERCENT, shooterMotor.ACHIEVABLE_MAX_TICKS_PER_SECOND, shooterMotor.getCorrectedVelocity(), shooterMotor.getAcceleration()));
         }
         telemetry.addData("speedy", shooterMotor.getVelocity());
     }
