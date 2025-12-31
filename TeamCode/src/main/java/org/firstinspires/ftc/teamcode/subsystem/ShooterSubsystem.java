79e46cdb66c1a010780bfc718170fd07a39e892c
awall <andrew.wall@stem2u.org>
2025-12-31 07:47:54 -0600

    adding pid and ff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/ShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/ShooterSubsystem.java
@@ -10,6 +10,8 @@ import static org.firstinspires.ftc.teamcode.util.RobotConfig.SHOOTER_FAR_PERCEN
 import android.util.Log;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.controller.wpilibcontroller.SimpleMotorFeedforward;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.arcrobotics.ftclib.hardware.motors.MotorGroup;
@@ -17,6 +19,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.teamcode.util.MathUtil;
 import org.firstinspires.ftc.teamcode.util.SpindexerMode;
 
 import java.util.List;
@@ -30,19 +33,21 @@ public class ShooterSubsystem extends SubsystemBase {
     private boolean running = false;
     private SpindexerMode currentMode;
     private double shootPercent = 5;
+    private PIDController shooterPID;
+    private SimpleMotorFeedforward shooterFeedForward;
 
     public ShooterSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
         this.telemetry = telemetry;
         shooterMotor = new MotorEx(hardwareMap, "shooterMotor");
         shootorMotor = new MotorEx(hardwareMap, "shootorMotor");
         groupOfGoop = new MotorGroup(shooterMotor, shootorMotor);
+        shooterPID = new PIDController(SHOOTER_P, SHOOTER_I, SHOOTER_D);
+        shooterFeedForward = new SimpleMotorFeedforward(SHOOTER_FF_S, SHOOTER_FF_V);
     }
 
     private void setUp() {
         groupOfGoop.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
-        groupOfGoop.setRunMode(Motor.RunMode.VelocityControl);
-        groupOfGoop.setVeloCoefficients(SHOOTER_P, SHOOTER_I, SHOOTER_D);
-        groupOfGoop.setFeedforwardCoefficients(SHOOTER_FF_S, SHOOTER_FF_V);
+        groupOfGoop.setRunMode(Motor.RunMode.RawPower);
     }
 
     public void stop() {
@@ -57,17 +62,25 @@ public class ShooterSubsystem extends SubsystemBase {
     public void shoot(double shootPercent) {
         this.shootPercent = shootPercent;
         running = true;
-        groupOfGoop.setVeloCoefficients(SHOOTER_P, SHOOTER_I, SHOOTER_D);
-        groupOfGoop.setFeedforwardCoefficients(SHOOTER_FF_S, SHOOTER_FF_V);
-
+        shooterPID.setPID(SHOOTER_P, SHOOTER_I, SHOOTER_D);
+        shooterPID.setSetPoint(1700); // just temporarily...
+        shooterFeedForward = new SimpleMotorFeedforward(SHOOTER_FF_S, SHOOTER_FF_V);
     }
 
     @Override
     public void periodic() {
         if (running) {
-            groupOfGoop.set(shootPercent);
-            Log.i("amps", String.format("%f, %f", shooterMotor.motorEx.getCurrent(CurrentUnit.AMPS), shootorMotor.motorEx.getCurrent(CurrentUnit.AMPS)));
-            Log.i("pct/MAX_TICKS/vel/accel", String.format("%f, %f, %f, %f", SHOOTER_FAR_PERCENT, groupOfGoop.ACHIEVABLE_MAX_TICKS_PER_SECOND, groupOfGoop.getVelocity(), shooterMotor.getAcceleration()));
+            double currentVelocity = shooterMotor.getCorrectedVelocity();
+            double pidOutput = shooterPID.calculate(currentVelocity);
+            double ffOutput = shooterFeedForward.calculate(currentVelocity, shooterMotor.getAcceleration());
+            double clamped = MathUtil.clamp(pidOutput + ffOutput, -1.0, 1.0);
+
+            Log.i("vel/ffOutput/pidOutput/clamped", String.format("%f, %f, %f, %f", currentVelocity, ffOutput, pidOutput, clamped));
+            telemetry.addData("ffOutput", ffOutput);
+            telemetry.addData("pidOutput", pidOutput);
+            telemetry.addData("shooter output", clamped);
+
+            groupOfGoop.set(clamped);
         }
         List<Double> velocities = groupOfGoop.getVelocities();
         telemetry.addData("speedy", velocities.get(0));
