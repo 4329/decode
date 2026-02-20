7c1cbe0c7ead3493a61fd25897670f432c901dba
cadetprogrammer <cadet@stem2u.org>
2026-02-19 18:51:18 -0600

    emoji

--- cleanBranches.sh
+++ cleanBranches.sh
@@ -1,18 +1,18 @@
 #/bin/bash
-cool_stuffs=($'\U01F920' $'\U01F4AF' $'\U01F496' $'\U01F44B' $'\U01F4A3' $'\U01F389' $'\U01F9E8' $'\U2728' $'\U01F38A')
+cool_stuffs=('🤠' '💯' '💖' '👋' '💣' '🎉' '🧨' '✨' '🎊')
 
 
 git fetch -p
 if [ $? -ne 0 ]
 then
-    printf "\n\U01F92E what? git fetch failed!  are you even in a git repo? cause it don't seem like `pwd` is.\n\n"
+    printf "\n😨 what? git fetch failed!  are you even in a git repo? cause it don't seem like `pwd` is.\n\n"
     exit 1
 fi
 
 git checkout dev
 if [ $? -ne 0 ]
 then
-    printf "\n\U01F92E oh my golly geepers gracious! I couldn't checkout dev.  I'm just stoppin now.\n\n"
+    printf "\n😱 oh my golly geepers gracious! I couldn't checkout dev.  I'm just stoppin now.\n\n"
     exit 1
 fi
 
@@ -24,4 +24,4 @@ do
 done
 git reset --hard origin/dev
 
-printf "\n\U01F4AA\U01F49E\U01F525\U01F332\U01F308\U01F499\U01F49A\U01F49B\U01F49C yayayayay your branches are super now.\n\n"
+printf "\n🔥 🦾 💞 🌲 🌈 🩷 🧡 💛 💚 💙 💜 yayayayay your branches are super now.\n\n"
